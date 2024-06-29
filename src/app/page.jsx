cfb9a77a0e463e086ec4e55cc4facf846b36b5ff
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-06-29 12:20:42 +0600

    Hero Component add

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div>
-      Home
+      <div className={styles.texts}>
+        <h1>Better design for your digital products.</h1>
+        <p>
+          Turning your idea into Reality. We bring together the teams from the global tech industry.
+        </p>
+      </div>
+      <div className={styles.imgContainer}></div>
+      <Image src="/hero.png" width={500} height={500} alt="" className={styles.img} />
     </div>
   );
 }
+
